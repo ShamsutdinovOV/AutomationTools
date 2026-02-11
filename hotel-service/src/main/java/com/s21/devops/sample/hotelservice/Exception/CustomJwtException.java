41e0941eb9ee43a2a1a47181b10c35f47ea6730e
ShamsutdinovOV <oleg_shamsutdino@mail.ru>
2026-02-11 16:11:40 +0300

    Delete hotel-service directory

--- hotel-service/src/main/java/com/s21/devops/sample/hotelservice/Exception/CustomJwtException.java
+++
@@ -1,7 +0,0 @@
-package com.s21.devops.sample.hotelservice.Exception;
-
-public class CustomJwtException extends Exception {
-    public CustomJwtException(String message) {
-        super(message);
-    }
-}
--- hotel-service/src/main/java/com/s21/devops/sample/hotelservice/Exception/HotelAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.s21.devops.sample.hotelservice.Exception;
-
-public class HotelAlreadyExistsException extends Exception {
-    public HotelAlreadyExistsException(String message) {
-        super(message);
-    }
-}
